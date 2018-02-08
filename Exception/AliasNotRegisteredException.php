d248ff4949460c76a17419187f43b8aa2bda956c
Rafal Mnich <rafal.mnich@msales.com>
2018-02-08 14:06:57 +0100

    test reviewme

--- Exception/AliasNotRegisteredException.php
+++ Exception/AliasNotRegisteredException.php
@@ -5,11 +5,5 @@ use Symfony\Component\DependencyInjection\Exception\ServiceNotFoundException;
 
 class AliasNotRegisteredException extends ServiceNotFoundException
 {
-    /**
-     * @param string $alias
-     */
-    public function __construct($alias)
-    {
-        parent::__construct($alias);
-    }
+
 }
